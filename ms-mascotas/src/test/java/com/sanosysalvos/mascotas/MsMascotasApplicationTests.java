41730f7d7eea090cb6dbc59c05bf50d0e558b8e9
Matilda Isabel Vargas Canseco <mat.vargasc@duocuc.cl>
2026-05-01 01:35:43 -0400

    Add files via upload

---
+++ ms-mascotas/src/test/java/com/sanosysalvos/mascotas/MsMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
