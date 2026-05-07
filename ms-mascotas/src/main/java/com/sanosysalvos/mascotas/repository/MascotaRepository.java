6099772f88d3bb647c3bcd7da721bbb2cfa70d7a
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-05-06 20:18:13 -0400

    feat: agrego repository de mascota

--- ms-mascotas/src/main/java/com/sanosysalvos/mascotas/repository/MascotaRepository.java
+++ ms-mascotas/src/main/java/com/sanosysalvos/mascotas/repository/MascotaRepository.java
@@ -1,5 +1,17 @@
 package com.sanosysalvos.mascotas.repository;
 
-public class MascotaRepository {
+import com.sanosysalvos.mascotas.model.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Long> {
+
+    List<Mascota> findByEstado(String estado);
+
+    List<Mascota> findByTipo(String tipo);
+
+    List<Mascota> findByTipoAndColor(String tipo, String color);
 }
