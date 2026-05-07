26aa828640bbed8261c9c1c406313710b1794f2b
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-05-06 23:29:35 -0400

    feat: agrego interface de servicio para bff mascotas

--- bff-sanosysalvos/src/main/java/com/sanosysalvos/bff/service/MascotasBffService.java
+++ bff-sanosysalvos/src/main/java/com/sanosysalvos/bff/service/MascotasBffService.java
@@ -1,5 +1,18 @@
 package com.sanosysalvos.bff.service;
 
-public class MascotasBffService {
+import com.sanosysalvos.bff.dto.MascotaDTO;
 
+public interface MascotasBffService {
+
+    MascotaDTO crearMascota(MascotaDTO mascotaDTO);
+
+    Object listarMascotas();
+
+    Object buscarMascotaPorId(Long id);
+
+    Object listarPorEstado(String estado);
+
+    Object actualizarMascota(Long id, MascotaDTO mascotaDTO);
+
+    void eliminarMascota(Long id);
 }
