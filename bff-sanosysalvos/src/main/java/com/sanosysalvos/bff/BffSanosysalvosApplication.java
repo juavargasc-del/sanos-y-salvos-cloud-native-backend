782e86fc7b9de1874368a49c645b854fc685b4f5
Juan fernando Vargas castillo <jua.vargasc@duocuc.cl>
2026-05-06 22:43:56 -0400

    Se sube la base para empezar bff

---
+++ bff-sanosysalvos/src/main/java/com/sanosysalvos/bff/BffSanosysalvosApplication.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSanosysalvosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffSanosysalvosApplication.class, args);
+	}
+
+}
---
+++ bff-sanosysalvos/src/test/java/com/sanosysalvos/bff/BffSanosysalvosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSanosysalvosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
