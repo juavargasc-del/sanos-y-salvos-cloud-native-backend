7a763294a4726068e378a1388bc58e7281141763
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-05-06 23:34:22 -0400

    feat: agrego controlador del bff mascotas

--- bff-sanosysalvos/src/main/java/com/sanosysalvos/bff/controller/MascotasBffController.java
+++ bff-sanosysalvos/src/main/java/com/sanosysalvos/bff/controller/MascotasBffController.java
@@ -1,5 +1,53 @@
 package com.sanosysalvos.bff.controller;
 
+import com.sanosysalvos.bff.dto.MascotaDTO;
+import com.sanosysalvos.bff.service.MascotasBffService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bff/mascotas")
+@RequiredArgsConstructor
 public class MascotasBffController {
 
+    private final MascotasBffService mascotasBffService;
+
+    @PostMapping
+    public ResponseEntity<MascotaDTO> crearMascota(@RequestBody MascotaDTO mascotaDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mascotasBffService.crearMascota(mascotaDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> listarMascotas() {
+        return ResponseEntity.ok(mascotasBffService.listarMascotas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> buscarMascotaPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(mascotasBffService.buscarMascotaPorId(id));
+    }
+
+    @GetMapping("/estado/{estado}")
+    public ResponseEntity<Object> listarPorEstado(@PathVariable String estado) {
+        return ResponseEntity.ok(mascotasBffService.listarPorEstado(estado));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> actualizarMascota(
+            @PathVariable Long id,
+            @RequestBody MascotaDTO mascotaDTO
+    ) {
+        return ResponseEntity.ok(
+                mascotasBffService.actualizarMascota(id, mascotaDTO)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarMascota(@PathVariable Long id) {
+        mascotasBffService.eliminarMascota(id);
+        return ResponseEntity.noContent().build();
+    }
 }
