e51aa97719fad0aa5aebb8f646454ac830902a6d
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-05-10 18:04:35 -0400

    feat: agrega modelo conceptual de coincidencia

--- ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/model/Coincidencia.java
+++ ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/model/Coincidencia.java
@@ -1,5 +1,20 @@
 package com.sanosysalvos.coincidencias.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Coincidencia {
 
+    private Long idMascotaPerdida;
+    private Long idMascotaEncontrada;
+
+    private String tipo;
+    private String raza;
+    private String color;
+
+    private String descripcion;
 }
