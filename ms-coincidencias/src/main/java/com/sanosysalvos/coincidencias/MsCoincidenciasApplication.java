b8cdbd991908686dd8ade9bfd565b709794adb17
Matilda Isabel Vargas Canseco <mat.vargasc@duocuc.cl>
2026-05-10 16:15:47 -0400

    agrego estructura inicial del ms coincidencias

---
+++ ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/MsCoincidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.coincidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoincidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCoincidenciasApplication.class, args);
+	}
+
+}
