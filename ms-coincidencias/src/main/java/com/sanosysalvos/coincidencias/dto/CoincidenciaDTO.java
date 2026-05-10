2f0d738e4350bdc9a2390a7005f72ed695dd1806
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-05-10 16:53:51 -0400

    feat: agrego dto de coincidencia

--- ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/dto/CoincidenciaDTO.java
+++ ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/dto/CoincidenciaDTO.java
@@ -1,5 +1,23 @@
 package com.sanosysalvos.coincidencias.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CoincidenciaDTO {
 
+    private Long idMascotaPerdida;
+    private Long idMascotaEncontrada;
+
+    private String nombreMascotaPerdida;
+    private String nombreMascotaEncontrada;
+
+    private String tipo;
+    private String raza;
+    private String color;
+
+    private String descripcion;
 }
