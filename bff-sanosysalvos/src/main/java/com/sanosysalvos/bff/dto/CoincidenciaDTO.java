e31eaf46f5c5d760591ec01b6e6c4b74c98db873
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-05-10 19:53:09 -0400

    feat: agrega dto de coincidencias para bff

---
+++ bff-sanosysalvos/src/main/java/com/sanosysalvos/bff/dto/CoincidenciaDTO.java
@@ -0,0 +1,23 @@
+package com.sanosysalvos.bff.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CoincidenciaDTO {
+
+    private Long idMascotaPerdida;
+    private Long idMascotaEncontrada;
+
+    private String nombreMascotaPerdida;
+    private String nombreMascotaEncontrada;
+
+    private String tipo;
+    private String raza;
+    private String color;
+
+    private String descripcion;
+}
