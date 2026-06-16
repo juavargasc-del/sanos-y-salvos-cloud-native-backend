3db1d9dd6f814f52989488c0cc44950ca5dd4f01
Juan Vargas <jua.vargasc@duocuc.cl>
2026-06-16 19:16:51 -0400

    test: se añade pruebas unitarias a geo

--- ms-geolocalizacion/src/test/java/com/sanosysalvos/geolocalizacion/MsGeolocalizacionApplicationTests.java
+++ ms-geolocalizacion/src/test/java/com/sanosysalvos/geolocalizacion/MsGeolocalizacionApplicationTests.java
@@ -3,7 +3,7 @@ package com.sanosysalvos.geolocalizacion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class MsGeolocalizacionApplicationTests {
 
     @Test
