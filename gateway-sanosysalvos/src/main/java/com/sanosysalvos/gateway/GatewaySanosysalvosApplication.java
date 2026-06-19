5baace85d4b478b0166ce41e92ba1b572ce45a6a
Juan Vargas <jua.vargasc@duocuc.cl>
2026-06-19 02:51:04 -0400

    feat(gateway): crear estructura base del API Gateway

---
+++ gateway-sanosysalvos/src/main/java/com/sanosysalvos/gateway/GatewaySanosysalvosApplication.java
@@ -0,0 +1,12 @@
+package com.sanosysalvos.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySanosysalvosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySanosysalvosApplication.class, args);
+	}
+}
