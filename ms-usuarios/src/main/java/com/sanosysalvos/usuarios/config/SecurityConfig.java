4d6c3b39a0d02b4c33cb21438160b389b44c7016
MatildaVargasC <mat.vargasc@duocuc.cl>
2026-06-29 03:07:36 -0400

    feature: se agrega perfectamente el swager con sus dependencias a todos los ms

--- ms-usuarios/src/main/java/com/sanosysalvos/usuarios/config/SecurityConfig.java
+++ ms-usuarios/src/main/java/com/sanosysalvos/usuarios/config/SecurityConfig.java
@@ -52,7 +52,14 @@ public class SecurityConfig {
                                 "/api/usuarios/login"
                         ).permitAll()
 
-                        // TODO LO DEMÁS PROTEGIDO
+                        // SWAGGER / OPENAPI
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+
                         .anyRequest().authenticated()
                 )
 
